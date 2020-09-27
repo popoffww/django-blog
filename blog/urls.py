79f052d7a95fead406a757ee05a861594874ddbf
popoffww <popoffww@yandex.ru>
2020-09-27 14:28:51 +0600

    Классы ListView DetailView

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('', HomeListView.as_view(), name='home'),
     path('posts/', PostListView.as_view(), name='posts'),
-    path('post/<str:slug>/', PostDetail.as_view(), name='post_detail'),
+    path('post/<str:slug>/', PostDetailView.as_view(), name='post_detail'),
     path('tags/', TagListView.as_view(), name='tags'),
     path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail'),
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
 from django.views.generic import View
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Post, Tag
 
@@ -13,14 +14,14 @@ class PostListView(ListView):
     model = Post
     queryset = Post.objects.filter(draft=False)
 
+class PostDetailView(DetailView):
+    model = Post
+    slug_field = 'slug'
+
+
 class TagListView(ListView):
     model = Tag
     queryset = Tag.objects.all()
-
-class PostDetail(View):
-    def get(self, request, slug):
-        post = Post.objects.get(slug__iexact=slug)
-        return render(request, 'blog/post_detail.html', {'post': post})
 
 class TagDetail(View):
     def get(self, request, slug):
