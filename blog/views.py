83f47a01529fe39d5d73a79a953d6e278a34cfbb
popoffww <popoffww@yandex.ru>
2020-10-02 17:34:55 +0600

    Оформление авторизации

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -21,22 +22,22 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     slug_field = 'slug'
+    raise_exception = True
 
-
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
     success_url = reverse_lazy('posts')
+    raise_exception = True
 
-
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/post_update.html'
     success_url = reverse_lazy('posts')
+    raise_exception = True
 
-
-class PostDeleteView(DeleteView):
+class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     success_url = reverse_lazy('posts')
 
@@ -52,22 +53,22 @@ class TagListView(ListView):
 class TagDetailView(DetailView):
     model = Tag
     slug_field = 'slug'
+    raise_exception = True
 
-
-class TagCreateView(CreateView):
+class TagCreateView(LoginRequiredMixin, CreateView):
     model = Tag
     form_class = TagForm
     success_url = reverse_lazy('tags')
+    raise_exception = True
 
-
-class TagUpdateView(UpdateView):
+class TagUpdateView(LoginRequiredMixin, UpdateView):
     model = Tag
     form_class = TagForm
     template_name = 'blog/tag_update.html'
     success_url = reverse_lazy('tags')
+    raise_exception = True
 
-
-class TagDeleteView(DeleteView):
+class TagDeleteView(LoginRequiredMixin, DeleteView):
     model = Tag
     success_url = reverse_lazy('tags')
 
