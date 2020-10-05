d66a2b045260848971a46c93e52adcb6ca3e990a
popoffww <popoffww@yandex.ru>
2020-10-05 12:35:56 +0600

    Реализация входа-выхода

--- blogengine/forms.py
+++ blogengine/forms.py
@@ -1,27 +1,23 @@
 from django import forms
-from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.hashers import check_password
 from django.core.exceptions import ValidationError
 
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField(label='Логин',
-                               widget=forms.TextInput(
-                                   attrs={'class': 'form-control'}
-                               ))
-    password = forms.CharField(label='Пароль',
-                               widget=forms.PasswordInput(
-                                   attrs={'class': 'form-control'}
-                               ))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     def clean(self, *args, **kwargs):
-        username = self.cleaned_data.get('username')
-        password = self.cleaned_data.get('password')
-
+        User = get_user_model()
+        username = self.cleaned_data.get('username').strip()
+        password = self.cleaned_data.get('password').strip()
         if username and password:
-            user = authenticate(username=username, password=password)
-            if not user:
-                raise forms.ValidationError('Такого пользователя не существует')
-            if not user.check_password(password):
-                raise forms.ValidationError('Неверный пароль')
-
-        return super(UserLoginForm, self).clean(*args, **kwargs)
+            qs = User.objects.filter(username=username)
+        if not qs.exists():
+            raise forms.ValidationError('Такого пользователя нет!')
+        if not check_password(password, qs[0].password):
+            raise forms.ValidationError('Пароль не верный!')
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise forms.ValidationError('Данный аккаунт отключен')
